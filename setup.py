5d86bb26ff0524bbe5c6412964dd1ad015880d81
Vemund Schøyen <vemund@live.com>
2025-01-31 12:00:25 +0100

    upgrades pypi version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='matplotlib-extensions',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     license='MIT',
     description='Natural extensions to Matplotlib for multidimensional plotting.',
